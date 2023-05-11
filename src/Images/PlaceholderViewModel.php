f53ab0f3ddc83ac8593bf29cedcb5c5f7bfccc83
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2023-05-11 13:32:48 +0100

    MVP

--- src/Images/PlaceholderViewModel.php
+++ src/Images/PlaceholderViewModel.php
@@ -4,8 +4,6 @@ namespace EngageInteractive\LaravelViewModels\Images;
 
 class PlaceholderViewModel extends ImageViewModel
 {
-    const URL_PREFIX = 'https://via.placeholder.com/';
-
     public function __construct(ImageDimensions $dimensions)
     {
         parent::__construct($dimensions);
@@ -13,12 +11,21 @@ class PlaceholderViewModel extends ImageViewModel
 
     protected function srcItem($dimension)
     {
-        return self::URL_PREFIX.$dimension['width'].'x'.$dimension['height'];
+        $svg = '<svg xmlns="http://www.w3.org/2000/svg" width=":width" height=":height" viewBox="0 0 :width :height">
+<rect fill="#ddd" width=":width" height=":height"/>
+<text fill="rgba(0,0,0,0.5)" font-family="sans-serif" font-size="30" dy="10.5" font-weight="bold" x="50%" y="50%" text-anchor="middle">:width×:height</text>
+</svg>';
+
+        $svg = str_replace(':width', $dimension['width'], $svg);
+        $svg = str_replace(':height', $dimension['height'], $svg);
+
+        return 'data:image/svg+xml,' . rawurlencode($svg);
     }
 
     protected function webpItem($dimension)
     {
-        return self::URL_PREFIX.$dimension['width'].'x'.$dimension['height'].'.webp/888/000';
+        // no different for placeholders
+        return $this->srcItem($dimension);
     }
 
     public function alt()

2f3a28b34054eb22ac59227182031559befbc675
Jason Stainton <jason@engageinteractive.co.uk>
2023-06-27 11:42:28 +0100

    Removes image models
    Use engageinteractive/image-models

--- src/helpers.php
+++ src/helpers.php
@@ -1,32 +1,13 @@
 <?php
 
-use EngageInteractive\LaravelViewModels\Images\PlaceholderViewModel;
-use EngageInteractive\LaravelViewModels\Images\ImageDimensions;
 use EngageInteractive\LaravelViewModels\ModelTransformer;
 
-if (! function_exists('image_placeholder')) {
+if (!function_exists('model_to_camel_case')) {
     /**
      * Returns an image placeholder model.
      *
      * @param string $key
-     * 
-     * @return array
-     */
-    function image_placeholder($key)
-    {
-        $dimensions = new ImageDimensions($key);
-        $model = new PlaceholderViewModel($dimensions);
-
-        return $model->getData();
-    }
-}
-
-if (! function_exists('model_to_camel_case')) {
-    /**
-     * Returns an image placeholder model.
      *
-     * @param string $key
-     * 
      * @return array
      */
     function model_to_camel_case($model)
